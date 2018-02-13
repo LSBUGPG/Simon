1b3af2233cb28774544607a8612374fbce7b05ad
Paul Sinnett <paul.sinnett@gmail.com>
2018-02-13 16:04:50 +0000

    detecting matches

--- Assets/RandomSequence.cs
+++ Assets/RandomSequence.cs
@@ -8,18 +8,19 @@ public class RandomSequence : MonoBehaviour
 	public AudioClip buzz;
 	public GameObject[] lights;
 	AudioSource source;
+	int buttonPressed;
 
 	void Start ()
 	{
 		source = GetComponent<AudioSource> ();
 
-		var sequence = CreateSequence (10);
+		var sequence = CreateSequence (1);
 		StartCoroutine (PlaySequence (sequence));
 	}
 
 	public void DetectButton(int button)
 	{
-		Debug.Log (button);
+		buttonPressed = button;
 	}
 
 	IEnumerator PlaySequence(List<int> sequence)
@@ -27,12 +28,64 @@ public class RandomSequence : MonoBehaviour
 		foreach (var item in sequence) {
 			source.clip = tone;
 			source.pitch = 0.8f + 0.1f * (float)item;
-			Debug.Log (item);
 			source.Play ();
 			lights [item].SetActive (true);
 			yield return new WaitForSeconds (0.5f);
 			lights [item].SetActive (false);
 			yield return new WaitForSeconds (0.5f);
+		}
+
+		buttonPressed = 4;
+		foreach (var item in sequence) {
+
+			float time = 0.0f;
+			while (time < 1.0f) {
+
+				if (item == buttonPressed) {
+					// correct answer
+					source.clip = tone;
+					source.pitch = 0.8f + 0.1f * (float)item;
+					source.Play ();
+					lights[item].SetActive(true);
+					yield return new WaitForSeconds (1.0f);
+					lights[item].SetActive(false);
+					break;
+				}
+				else if (item != buttonPressed && buttonPressed != 4) {
+					// wrong answer...
+					foreach (var light in lights) {
+						light.SetActive (true);
+					}
+					source.clip = buzz;
+					source.Play ();
+					yield return new WaitForSeconds (1.0f);
+					foreach (var light in lights) {
+						light.SetActive (false);
+					}
+					break;
+				}
+				time = time + Time.deltaTime;
+				yield return null;
+			}
+
+			if (buttonPressed == 4) {
+				// too late...
+				foreach (var light in lights) {
+					light.SetActive (true);
+				}
+				source.clip = buzz;
+				source.Play ();
+				yield return new WaitForSeconds (1.0f);
+				foreach (var light in lights) {
+					light.SetActive (false);
+				}
+				break;
+			}
+
+
+
+
+			yield return new WaitForSeconds (1.0f);
 		}
 	}
 
