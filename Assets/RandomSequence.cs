e052b5aa095f936784f34ec82bca73f8179f075f
Paul Sinnett <paul.sinnett@gmail.com>
2018-02-13 14:39:00 +0000

    added a sequence

---
+++ Assets/RandomSequence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSequence : MonoBehaviour
+{
+	void Start ()
+	{
+		var sequence = CreateSequence (10);
+		foreach (var item in sequence) {
+			Debug.Log (item);
+		}
+	}
+
+	List<int> CreateSequence(int n)
+	{
+		List <int> sequence = new List<int> ();
+		for (int i = 0; i < n; i++) {
+			sequence.Add (Random.Range (0, 4));
+		}
+
+		return sequence;
+	}
+
+	void Update ()
+	{
+		
+	}
+}
