079c56273fe93101119bb7185d2c7917eaa0f020
Paul Sinnett <paul.sinnett@gmail.com>
2018-02-13 15:26:01 +0000

    added lights and sounds

--- Assets/RandomSequence.cs
+++ Assets/RandomSequence.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class RandomSequence : MonoBehaviour
 {
+	public AudioClip tone;
+	public AudioClip buzz;
+	public GameObject[] lights;
+	AudioSource source;
+
 	void Start ()
 	{
+		source = GetComponent<AudioSource> ();
+
 		var sequence = CreateSequence (10);
+		StartCoroutine (PlaySequence (sequence));
+	}
+
+	IEnumerator PlaySequence(List<int> sequence)
+	{
 		foreach (var item in sequence) {
+			source.clip = tone;
+			source.pitch = 0.8f + 0.1f * (float)item;
 			Debug.Log (item);
+			source.Play ();
+			lights [item].SetActive (true);
+			yield return new WaitForSeconds (0.5f);
+			lights [item].SetActive (false);
+			yield return new WaitForSeconds (0.5f);
 		}
 	}
 
